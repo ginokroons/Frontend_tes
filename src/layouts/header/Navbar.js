f7a8101a060a39131fd36db7e88098291dbe1122
ginokroons <ginokroons01@gmail.com>
2022-09-23 16:46:33 +0700

    update navbar

--- src/layouts/header/Navbar.js
+++ src/layouts/header/Navbar.js
@@ -1,25 +1,36 @@
 import React from 'react'
 import { AppBar, Button, Toolbar, Typography, Stack, Box } from '@mui/material'
+import { KeyboardArrowDown } from '@mui/icons-material';
 
 const Navbar = () => {
-  return (
-    <AppBar position='static'>
-        <Toolbar>
-            <Box
-                component="img"
-                alt="logo"
-                src="./logo-wordmark.png"
-            />
-            <Typography variant='h6' component='div' sx={{flexGrow: 1}}>
-                I3gis
-            </Typography>
-            <Stack direction='row' spacing={2}>
-                <Button variant='outlined' color='inherit'>Image Scan</Button>
-                <Button variant='outlined' color='inherit'>Configuration Scan</Button>
-            </Stack>
-        </Toolbar>
-    </AppBar>
-  )
+
+    const [anchorEl, setAnchorEl] = React.useState(null);
+    const open = Boolean(anchorEl);
+    const handleClick = (event) => {
+        setAnchorEl(event.currentTarget);
+    };
+    const handleClose = () => {
+        setAnchorEl(null);
+    };
+
+    return (
+        <AppBar position='static'>
+            <Toolbar>
+                <Box
+                    component="img"
+                    alt="logo"
+                    src="./logo-wordmark.png"
+                />
+                <Stack direction='row' spacing={2}>
+                    <Typography variant='h6' component='div' sx={{flexGrow: 1}}>
+                        I3gis
+                    </Typography>
+                    <Button color='inherit' endIcon={<KeyboardArrowDown/>}>Image Scan</Button>
+                    <Button color='inherit' endIcon={<KeyboardArrowDown/>}>Configuration Scan</Button>
+                </Stack>
+            </Toolbar>
+        </AppBar>
+    )
 }
 
 export default Navbar
